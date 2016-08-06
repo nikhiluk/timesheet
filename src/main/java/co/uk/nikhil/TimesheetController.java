232df2896a8c5c211d4834a26c6a370beceb69f8
nikhilnz <nikhilsharma@hotmail.com>
2016-08-06 22:46:41 +0100

    Add rest controller.

---
+++ src/main/java/co/uk/nikhil/TimesheetController.java
@@ -0,0 +1,26 @@
+package co.uk.nikhil;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TimesheetController   {
+
+    @Autowired
+    private TimesheetService timesheetService;
+
+    @RequestMapping(value = "/add/today", method = RequestMethod.GET)
+    @ResponseStatus(HttpStatus.OK)
+    public String addToday() {
+        timesheetService.addToday();
+        return "";
+    }
+
+    public void setTimesheetService(TimesheetService timesheetService) {
+        this.timesheetService = timesheetService;
+    }
+}
---
+++ src/main/test/co/uk/nikhil/TimesheetControllerTest.java
@@ -0,0 +1,45 @@
+package co.uk.nikhil;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpMethod;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.web.client.RestTemplate;
+
+import java.sql.Date;
+import java.util.HashMap;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = {"classpath:spring.xml"})
+public class TimesheetControllerTest {
+
+    private RestTemplate restTemplate;
+
+    @Autowired
+    JdbcTemplate jdbcTemplate;
+
+    @Before
+    public void setup() {
+        restTemplate = new RestTemplate();
+        this.jdbcTemplate.execute("delete from days_worked");
+    }
+
+    @Test
+    public void addToday() {
+        String url = "http://localhost:8080/add/today";
+        restTemplate.exchange(url, HttpMethod.GET, null, String.class, new HashMap<String, Object>());
+
+        int count = jdbcTemplate.queryForInt("select count(*) from days_worked");
+        assertThat(count, is(1));
+
+        Date date = (Date) jdbcTemplate.queryForObject("select day from days_worked", Date.class);
+        assertThat(date, is(new Date(date.getTime())));
+    }
+}
