385cc0e10f5eabeff81d1458b452cfb24a8b8c3c
nikhilnz <nikhilsharma@hotmail.com>
2016-08-12 07:57:49 +0100

    web app

--- src/main/java/co/uk/nikhil/TimesheetController.java
+++ src/main/java/co/uk/nikhil/TimesheetController.java
@@ -2,21 +2,30 @@ package co.uk.nikhil;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
-@RestController
+@Controller
 public class TimesheetController   {
 
     @Autowired
     private TimesheetService timesheetService;
 
-    @RequestMapping(value = "/add/today", method = RequestMethod.GET)
+    @RequestMapping(value = "/add/today", method = RequestMethod.POST)
     @ResponseStatus(HttpStatus.OK)
     public void addToday() {
         timesheetService.addToday();
+    }
+
+    @RequestMapping(value = "/timesheet")
+    @ResponseStatus(HttpStatus.OK)
+    public ModelAndView timesheet() {
+        ModelAndView modelAndView = new ModelAndView("timesheet");
+        return modelAndView;
     }
 
     public void setTimesheetService(TimesheetService timesheetService) {
