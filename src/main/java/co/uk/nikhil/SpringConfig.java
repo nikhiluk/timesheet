bd891d7c6e86c16916dee494fc3617c9ba0f55b5
nikhilsharma <nikhilsharma0304@gmail.com>
2017-02-01 16:25:03 +0000

    use property file.

--- src/main/java/co/uk/nikhil/SpringConfig.java
+++ src/main/java/co/uk/nikhil/SpringConfig.java
@@ -3,22 +3,39 @@ package co.uk.nikhil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 import javax.sql.DataSource;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 
 @Configuration
+@PropertySource("classpath:app.properties")
 @EnableWebMvc
 public class SpringConfig {
+
+    @Autowired
+    Environment env;
 
     @Bean
     @Autowired
     public TimesheetService timesheetService(TimesheetDao timesheetDao) {
         TimesheetService timesheetService = new TimesheetService();
         timesheetService.setTimesheetDao(timesheetDao);
-        timesheetService.setCurrentDateService(new CurrentDateService() );
+        String currentDate = env.getProperty("current.date");
+        if (currentDate != null && !currentDate.isEmpty()) {
+            try {
+                timesheetService.setCurrentDateService(new CurrentDateService(getDateToTest(currentDate)));
+            } catch (ParseException e) {
+                timesheetService.setCurrentDateService(new CurrentDateService());
+            }
+        } else {
+            timesheetService.setCurrentDateService(new CurrentDateService());
+        }
         return timesheetService;
     }
 
@@ -40,10 +57,15 @@ public class SpringConfig {
     public DataSource dataSource() {
         DriverManagerDataSource driverManagerDataSource = new DriverManagerDataSource();
         driverManagerDataSource.setDriverClassName("com.mysql.jdbc.Driver");
-        driverManagerDataSource.setUrl("jdbc:mysql://localhost:3306/timesheet_dev");
+        String dbName = env.getProperty("database.name");
+        driverManagerDataSource.setUrl("jdbc:mysql://localhost:3306/" + dbName);
         driverManagerDataSource.setUsername("root");
 //        driverManagerDataSource.setPassword(environment.getRequiredProperty("jdbc.password"));
 
         return driverManagerDataSource;
+    }
+
+    private java.util.Date getDateToTest(String dateString) throws ParseException {
+        return new SimpleDateFormat("dd/MM/yyyy").parse(dateString);
     }
 }
