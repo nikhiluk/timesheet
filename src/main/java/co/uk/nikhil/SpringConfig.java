5cd5dd407578795c4f7726c2387871c5bee9c1f6
nikhilsharma <nikhilsharma0304@gmail.com>
2017-02-01 08:38:28 +0000

    annotation config

---
+++ src/main/java/co/uk/nikhil/SpringConfig.java
@@ -0,0 +1,49 @@
+package co.uk.nikhil;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+import javax.sql.DataSource;
+
+@Configuration
+@EnableWebMvc
+public class SpringConfig {
+
+    @Bean
+    @Autowired
+    public TimesheetService timesheetService(TimesheetDao timesheetDao) {
+        TimesheetService timesheetService = new TimesheetService();
+        timesheetService.setTimesheetDao(timesheetDao);
+        timesheetService.setCurrentDateService(new CurrentDateService() );
+        return timesheetService;
+    }
+
+    @Bean
+    @Autowired
+    public TimesheetDao timesheetDao(JdbcTemplate jdbcTemplate) {
+        TimesheetDao timesheetDao = new TimesheetDao();
+        timesheetDao.setJdbcTemplate(jdbcTemplate);
+        return timesheetDao;
+    }
+
+    @Bean
+    @Autowired
+    public JdbcTemplate jdbcTemplate(DataSource dataSource) {
+        return new JdbcTemplate(dataSource);
+    }
+
+    @Bean
+    public DataSource dataSource() {
+        DriverManagerDataSource driverManagerDataSource = new DriverManagerDataSource();
+        driverManagerDataSource.setDriverClassName("com.mysql.jdbc.Driver");
+        driverManagerDataSource.setUrl("jdbc:mysql://localhost:3306/timesheet_dev");
+        driverManagerDataSource.setUsername("root");
+//        driverManagerDataSource.setPassword(environment.getRequiredProperty("jdbc.password"));
+
+        return driverManagerDataSource;
+    }
+}
