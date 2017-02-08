d94cfcc3b8b4539a0cbc37544f75a542f1323756
nikhilsharma <nikhilsharma0304@gmail.com>
2017-02-08 18:11:57 +0000

    refactor to have full url in path.

--- src/main/java/co/uk/nikhil/TimesheetController.java
+++ src/main/java/co/uk/nikhil/TimesheetController.java
@@ -8,6 +8,9 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
 @RestController
 public class TimesheetController {
 
@@ -16,24 +19,29 @@ public class TimesheetController {
 
     @RequestMapping(value = "/today", method = RequestMethod.POST)
     @ResponseStatus(HttpStatus.OK)
-    public ModelAndView addToday(ModelAndView modelAndView) {
+    public void addToday(HttpServletResponse response) {
         timesheetService.addToday();
-        modelAndView.setViewName("/timesheet.do");
-        return modelAndView;
-
+        redirectToMainPage(response);
     }
 
     @RequestMapping(value = "/month-till-today", method = RequestMethod.POST)
     @ResponseStatus(HttpStatus.OK)
-    public ModelAndView addMonthTillToday(ModelAndView modelAndView) {
+    public void addMonthTillToday(HttpServletResponse response) {
         timesheetService.addMonthTillToday();
-        modelAndView.setViewName("/timesheet.do");
-        return modelAndView;
+        redirectToMainPage(response);
     }
 
     @RequestMapping(value = "/month-till-today", method = RequestMethod.GET)
     @ResponseStatus(HttpStatus.OK)
     public String getMonthTillToday() {
         return timesheetService.getDaysWorkedThisMonth().toString();
+    }
+
+    private void redirectToMainPage(HttpServletResponse response) {
+        try {
+            response.sendRedirect("/timesheetApp/timesheet.do");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
