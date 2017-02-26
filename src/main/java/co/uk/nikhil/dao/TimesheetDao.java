7cd3478d3adf8635dd9e9b033d716ee2df35d9de
nikhilsharma <nikhilsharma0304@gmail.com>
2017-02-26 19:29:19 +0000

    use postgres.

--- src/main/java/co/uk/nikhil/dao/TimesheetDao.java
+++ src/main/java/co/uk/nikhil/dao/TimesheetDao.java
@@ -3,30 +3,31 @@ package co.uk.nikhil.dao;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
 import org.springframework.stereotype.Service;
 
 import java.util.Date;
-@Service
+@Repository
 public class TimesheetDao {
 
     @Autowired
     private JdbcTemplate jdbcTemplate;
 
     public void addDate(Date date) {
-        String sql = "insert into timesheet_dev.days_worked values (?)";
+        String sql = "insert into timesheet.days_worked values (?)";
         jdbcTemplate.update(sql, new Object[]{new java.sql.Date(date.getTime())});
     }
 
     public boolean dateExists(Date date) {
-        int count = jdbcTemplate.queryForInt("select count(*) from timesheet_dev.days_worked where day = ?", new Object[]{new java.sql.Date(date.getTime())});
+        int count = jdbcTemplate.queryForInt("select count(*) from timesheet.days_worked where day = ?", new Object[]{new java.sql.Date(date.getTime())});
         return count == 1;
     }
 
     public Integer getDaysByMonthAndYear(int month, int year) {
-        return jdbcTemplate.queryForInt("select count(*) from timesheet_dev.days_worked where date_part('month', day) = ? AND date_part('year', day) = ?", month, year);
+        return jdbcTemplate.queryForInt("select count(*) from timesheet.days_worked where date_part('month', day) = ? AND date_part('year', day) = ?", month, year);
     }
 
     public void deleteDaysForMonth(int month) {
-        jdbcTemplate.update("delete from timesheet_dev.days_worked where date_part('month', day) = ?", month);
+        jdbcTemplate.update("delete from timesheet.days_worked where date_part('month', day) = ?", month);
     }
 }
